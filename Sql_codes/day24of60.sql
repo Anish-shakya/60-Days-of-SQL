20da005844ef05f3b1583254e6d7d47a273b2c4c
Anish Shakya <shakyaanish69@gmail.com>
2023-07-12 11:45:17 +0545

    day24of60

---
+++ Sql_codes/day24of60.sql
@@ -0,0 +1,16 @@
+SELECT 
+	-- Select country ID, date, home, and away goals from match
+	main.country_id,
+    main.date,
+    main.home_goal, 
+    main.away_goal
+FROM match AS main
+WHERE 
+	-- Filter the main query by the subquery
+	(home_goal + away_goal) > 
+        (SELECT AVG((sub.home_goal + sub.away_goal) * 3)
+         FROM match AS sub
+         -- Join the main query to the subquery in WHERE
+         WHERE main.country_id = sub.country_id);
+
+
